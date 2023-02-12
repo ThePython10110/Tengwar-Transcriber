92d218f13466c2bc5607383a078f85efebb2d91c
ThePython <nolanjbowers@gmail.com>
2023-02-11 17:49:54 -0800

    Converted RegEx iterator to array

--- tengwartranscriber.js
+++ tengwartranscriber.js
@@ -41,7 +41,7 @@ function toCharStrings(inputString) {
 }
 
 function splitCharStrings(inputString) {
-	return inputString.matchAll(/^[^\|\{\[]+?(?=[\|\{\[])|(?<=[\|\{\[])[^\|\}\]]+?$|\|.+?\||\{.+?\}|\[.+?\]|(?<=[\|\}\]]).+?(?=[\|\{\[])|^.+?$/g)
+	return [...inputString.matchAll(/^[^\|\{\[]+?(?=[\|\{\[])|(?<=[\|\{\[])[^\|\}\]]+?$|\|.+?\||\{.+?\}|\[.+?\]|(?<=[\|\}\]]).+?(?=[\|\{\[])|^.+?$/g)]
 } //that regex took way too long...
 
 function toTengwar(inputString) {
